155c327c82b860e97de100ec8b3dbf4afbeb2378
laura <lg8104@gmail.com>
2025-02-17 16:43:59 -0700

    Updating login page, stylsheet, and script file

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-
-
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = document.querySelector(".canvas").style.width = 80%;
+canvas.height = 80%;
+ctx.beginPath();
+ctx.fillStyle = "#60affe";
+ctx.arc(50, 50, 50, 0, 2 * Math.PI);
+ctx.fill();
